839c47d0ef1938cbdd85806e2fda4f23a52907be
João Victor <elietejvc@hotmail.com>
2025-04-03 20:06:20 -0300

    fix: melhorando path

--- src/app/(auth)/cadastro/page.tsx
+++ src/app/(auth)/cadastro/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { usuarioService } from '@/lib/api/usuarios';
+import { usuarioService } from '@/lib/request/usuarios';
 
 export default function Cadastro() {
   const [formData, setFormData] = useState({
