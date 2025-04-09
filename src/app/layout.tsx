86522124d7f0e7a9a9a6d2da1451e71f55e72bf5
João Victor <elietejvc@hotmail.com>
2025-04-09 01:49:14 -0300

    refactor: corrigindo estrutura de pasta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
+import { Header } from "@/lib/components/header";
+import { Footer } from "@/lib/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
