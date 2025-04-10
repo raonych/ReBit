94f65793094593f42f2ce39ae7f1529c298f2982
raonych <raonych@gmail.com>
2025-04-10 01:48:55 -0300

    feat: adiciona endpoint GET produtos recentes

--- src/lib/services/produtoService.ts
+++ src/lib/services/produtoService.ts
@@ -24,3 +24,25 @@ export async function CadastrarProduto(body: any){
         return { status: 500, data: { error: "Erro interno do servidor" } };
     }
 }
+
+export async function ExibirProdutosRecentes(){
+    try{
+        const produtosRecentes = await prisma.produto.findMany({
+            orderBy: {
+              criadoEm: 'desc'
+            },
+            take: 6
+          })
+
+
+          if (produtosRecentes.length === 0) {
+            return { status: 200, data: { message: "Nenhum produto encontrado"} };
+          }
+
+          return { status: 200, data: produtosRecentes };
+    }
+    catch (error) {
+        console.error("Erro ao retornar produtos:", error)
+        return { status: 500, data: { error: "Erro interno do servidor" } };
+    }
+}
