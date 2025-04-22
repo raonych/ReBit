e1cd24692dd3a654c9ec1f2ee2ec09f064de27a2
raonych <raonych@gmail.com>
2025-04-22 00:53:06 -0300

    feat:adicionando rotas exibe perfil usuario e att perfil usuario

---
+++ src/lib/services/usuarioService.ts
@@ -0,0 +1,46 @@
+import { prisma } from '@/lib/prisma';
+import { usuarioUpdateSchema } from '../validators/usuario';
+export async function exibePerfil(userId: number){
+    try{
+        const perfil = await prisma.usuario.findUnique({
+            where:{id: userId}
+        })
+        if(!perfil){
+            return{status: 404, data:{message:"usuário não encontrado"}}
+        }
+
+        const {senha:_, ...perfilSemSenha} = perfil;
+
+        return{status:200, data:perfilSemSenha};
+
+    }catch(error){
+        console.error("Erro ao retornar perfil:", error)
+        return { status: 500, data: { error: "Erro interno do servidor" } };
+    }
+}
+
+export async function atualizaPerfil(body: any, userId: number){
+    try{
+        const validatedData = usuarioUpdateSchema.parse(body);
+
+        const perfil = await prisma.usuario.findUnique({
+            where:{id: userId}
+        })
+        if(!perfil){
+            return{status: 404, data:{message:"usuário não encontrado"}}
+        }
+
+        const perfilAtualizado = await prisma.usuario.update({
+            where:{id: userId},
+            data: validatedData
+        })
+
+        const {senha:_, ...perfilSemSenha} = perfilAtualizado;
+
+        return{status:200, data:perfilSemSenha}
+
+    }catch(error){
+        console.error("Erro ao atualizar perfil:", error)
+        return { status: 500, data: { error: "Erro interno do servidor" } };
+    }
+}
